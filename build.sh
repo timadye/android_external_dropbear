4612767f28f7b4ac077bc1415bb6922598e6a042
adye <T.J.Adye@rl.ac.uk>
2011-02-22 04:18:46 +0000

    build.sh also builds sftp-server

--- build.sh
+++ build.sh
@@ -2,4 +2,7 @@ set -x
 ./configure --prefix=/data/local --host=arm-linux-androideabi CFLAGS='-march=armv7-a -mfloat-abi=softfp -DANDROID_CHANGES' --disable-utmp --disable-wtmp
 make          PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp"
 make strip    PROGRAMS="dropbear dbclient dropbearkey dropbearconvert scp"
-tar --owner=0 --group=0 -zcf dropbear.tar.gz dropbear dbclient dropbearkey dropbearconvert scp
+cd sftp-server
+make
+cd ..
+tar --owner=0 --group=0 -zcf dropbear.tar.gz dropbear dbclient dropbearkey dropbearconvert scp -C sftp-server sftp-server
